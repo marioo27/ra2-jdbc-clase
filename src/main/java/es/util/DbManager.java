18eea3775a2874538ee7c7bacd27d2387627dbee
Mario Garcia <mariogarrod@gmail.com>
2025-11-26 09:06:10 +0100

    extension y arbol

---
+++ src/main/java/es/util/DbManager.java
@@ -0,0 +1,18 @@
+package es.util;
+
+import java.sql.Connection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Mario Garcia
+ * @version 1.0.0
+ */
+public class DbManager {
+    private static final Logger LOF = LoggerFactory.getLogger(DbManager.class);
+
+    private Connection con = null;
+    
+
+}
